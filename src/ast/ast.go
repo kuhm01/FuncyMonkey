69d7d8764db8c63bfe7f4165c2e08c353b55e71c
kuhm01 <kuhm01@naver.com>
2022-02-04 14:20:21 +0900

    Create ast.go

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -3,3 +3,22 @@ package ast
 func Hello() string {
 	return "Hello! This is ast.go! in ast package"
 }
+
+//Hello! Package
+
+type Node interface {
+	TokenLiteral() string
+	String() string
+}
+
+/*Statement 명령문*/
+type Statement interface {
+	Node
+	statementNode()
+}
+
+/*Expression 표현식*/
+type Expression interface {
+	Node
+	expressionNode()
+}
