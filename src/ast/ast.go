3504fe20f849e9972bfe0901c170d9a93e36fb63
kuhm01 <kuhm01@naver.com>
2022-02-04 15:32:58 +0900

    Update ast.go

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "bytes"
+
 func Hello() string {
 	return "Hello! This is ast.go! in ast package"
 }
@@ -21,4 +23,28 @@ type Statement interface {
 type Expression interface {
 	Node
 	expressionNode()
+}
+
+/*Program Node ast의 첫 노드. 명령문을 내포*/
+type Program struct {
+	/*Set of Statement*/
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
 }
